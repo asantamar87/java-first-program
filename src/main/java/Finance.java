3cc84da1411b802a224d5a36d66377c87f56429f
aldosantamaria <asantama@jaumebalmes.net>
2022-10-12 13:28:18 +0200

    Entrega Final. Fix Finance v1.2

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -24,40 +24,5 @@ public class Finance {
             System.exit(-1);
         }
 
-        final boolean isValidCommand = validateCommandArguments(args);
-        if (!isValidCommand) {
-            System.out.println(commandsToUsage.get(args[0]));
-            System.exit(-1);
-        }
-
-        executeCommand(command, Arrays.copyOfRange(args, 1, args.length));
-    }
-
-    private static void executeCommand(String command, String[] arguments) {
-        switch (command) {
-            case BEST_LOAN_RATES:
-                System.out.println("Finding best loan rates ..."); // for testing just log
-                BestLoanRates.main(arguments); // for another test call these methods and have executeCommand add to main method
-                return;
-            case SAVINGS_CALCULATOR:
-                System.out.println("Finding your net savings ...");
-                SavingsCalculator.main(arguments);
-                return;
-            case MORTGAGE_CALCULATOR:
-                System.out.println("Finding your monthly payment ...");
-                MortgageCalculator.main(arguments);
-        }
-    }
-
-    private static boolean validateCommandArguments(String[] args) {
-        switch (args[0]) {
-            case BEST_LOAN_RATES:
-                return args.length == 1;
-            case SAVINGS_CALCULATOR:
-                return args.length == 3;
-            case MORTGAGE_CALCULATOR:
-                return args.length == 4;
-        }
-        return false;
     }
 }
